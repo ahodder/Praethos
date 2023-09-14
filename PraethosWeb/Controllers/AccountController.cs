c152d62840e06971f67db0bb29e178e6d9344fbd
Andrew Hodder <aedonetlira@gmail.com>
2023-09-13 19:08:55 -0600

    Initial commit

---
+++ PraethosWeb/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PraethosWeb.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
---
+++ PraethosWeb/Models/PraethosUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PraethosWeb.Models;
+
+public class PraethosUser : IdentityUser
+{
+    
+}
